df7b309e21c62605c0d3ea28c4f09b22002635ff
Mohit Mutha <mohit.mutha@siemens.com>
2019-07-15 17:02:41 +0530

    Initial commit

---
+++ src/main/java/com/mm/poc/config/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,117 @@
+package com.mm.poc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+  @Autowired
+  @Qualifier("authenticationManagerBean")
+  private AuthenticationManager authenticationManager;
+
+  @Override
+  public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+  }
+
+  @Override
+  public void configure(final ClientDetailsServiceConfigurer clients) throws Exception { // @formatter:off
+    clients.inMemory()
+            .withClient("sampleClientId")
+            .authorizedGrantTypes("implicit")
+            .scopes("read", "write", "foo", "bar")
+            .autoApprove(false)
+            .accessTokenValiditySeconds(3600)
+            .redirectUris("http://localhost:8083/","http://localhost:8086/")
+            .and()
+            .withClient("fooClientIdPassword")
+            .secret(passwordEncoder().encode("secret"))
+            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials")
+            .scopes("foo", "read", "write")
+            .accessTokenValiditySeconds(3600)       // 1 hour
+            .refreshTokenValiditySeconds(2592000)  // 30 days
+            .redirectUris("http://www.example.com","http://localhost:8089/","http://localhost:8080/login/oauth2/code/custom","http://localhost:8080/ui-thymeleaf/login/oauth2/code/custom", "http://localhost:8080/authorize/oauth2/code/bael", "http://localhost:8080/login/oauth2/code/bael")
+            .and()
+            .withClient("barClientIdPassword")
+            .secret(passwordEncoder().encode("secret"))
+            .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+            .scopes("bar", "read", "write")
+            .accessTokenValiditySeconds(3600)       // 1 hour
+            .refreshTokenValiditySeconds(2592000)  // 30 days
+            .and()
+            .withClient("testImplicitClientId")
+            .authorizedGrantTypes("implicit")
+            .scopes("read", "write", "foo", "bar")
+            .autoApprove(true)
+            .redirectUris("http://www.example.com");
+  } // @formatter:on
+
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore(redisConnectionFactory()));
+    defaultTokenServices.setSupportRefreshToken(true);
+    return defaultTokenServices;
+  }
+
+
+  @Override
+  public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+    tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+    endpoints.tokenStore(tokenStore(redisConnectionFactory())).tokenEnhancer(tokenEnhancerChain).authenticationManager(authenticationManager);
+  }
+
+  @Bean
+  public TokenStore tokenStore(RedisConnectionFactory redisConnectionFactory) {
+    return new RedisTokenStore(redisConnectionFactory);
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    KeyStoreKeyFactory keyStoreKeyFactory =
+            new KeyStoreKeyFactory(new ClassPathResource("poc.jks"), "changeit".toCharArray());
+    converter.setKeyPair(keyStoreKeyFactory.getKeyPair("poc"));
+    return converter;
+  }
+
+  @Bean
+  public TokenEnhancer tokenEnhancer() {
+    return new CustomTokenEnhancer();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory();
+  }
+}
